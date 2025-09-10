f2499abbc4f32201c17097aa2a934dc415e04f3c
Mr Robot <mrrobotcodedev@gmail.com>
2025-09-10 15:36:38 -0300

    feat(environment): add Tasker environment to environment management service

--- src/services/EnvironmentManager.js
+++ src/services/EnvironmentManager.js
@@ -1,18 +1,21 @@
 const EnvironmentManager = (() => {
-  const WebEnvironment = (() => {
-    const NAME = "web";
-    const WORK_DIR = ".";
-    const LANG_CODE = "pt-BR";
-    const ICON_PATH = "src/assets/icons/";
-
-    const STORAGE_KEYS = {
-      routines: "@routine-flow:routines",
-      settings: "@routine-flow:settings",
-      categories: "@routine-flow:categories"
+  const WebEnvironment = () => {
+    const config = {
+      name: "web",
+      workDir: ".",
+      langCode: "pt-BR",
+      iconPath: "src/assets/icons/",
+      storageKeys: {
+        routines: "@routine-flow:routines",
+        settings: "@routine-flow:settings",
+        categories: "@routine-flow:categories"
+      }
     };
 
     async function loadLang(code) {
-      const response = await fetch(`${WORK_DIR}/src/locales/${code}.json`);
+      const response = await fetch(
+        `${config.workDir}/src/locales/${code}.json`
+      );
       return await response.json();
     }
 
@@ -25,27 +28,27 @@ const EnvironmentManager = (() => {
     }
 
     function getCategories() {
-      return localStorage.getItem(STORAGE_KEYS.categories);
+      return localStorage.getItem(config.storageKeys.categories);
     }
 
     function saveCategories(data) {
-      localStorage.setItem(STORAGE_KEYS.categories, data);
+      localStorage.setItem(config.storageKeys.categories, data);
     }
 
     function getRoutines() {
-      return localStorage.getItem(STORAGE_KEYS.routines);
+      return localStorage.getItem(config.storageKeys.routines);
     }
 
     function saveRoutines(data) {
-      localStorage.setItem(STORAGE_KEYS.routines, data);
+      localStorage.setItem(config.storageKeys.routines, data);
     }
 
     function getSettings() {
-      return localStorage.getItem(STORAGE_KEYS.settings);
+      return localStorage.getItem(config.storageKeys.settings);
     }
 
     function saveSettings(settings) {
-      localStorage.setItem(STORAGE_KEYS.settings, settings);
+      localStorage.setItem(config.storageKeys.settings, settings);
     }
 
     function exit() {
@@ -53,10 +56,7 @@ const EnvironmentManager = (() => {
     }
 
     return {
-      name: NAME,
-      langCode: LANG_CODE,
-      workDir: WORK_DIR,
-      iconPath: ICON_PATH,
+      ...config,
       loadLang,
       loadApps,
       loadTasks,
@@ -68,10 +68,99 @@ const EnvironmentManager = (() => {
       saveSettings,
       exit
     };
-  })();
+  };
+
+  const TaskerEnvironment = () => {
+    const config = {
+      name: "tasker",
+      langCode: tk.local("sys_lang_code") || "en",
+      workDir: "%project_path",
+      iconPath: "content://net.dinglisch.android.taskerm.iconprovider//app/"
+    };
+
+    const state = { priority: 50 };
+
+    function validateData(data) {
+      const isValid = !data.match(/^%[a-z_]+/);
+      return isValid ? data : null;
+    }
+
+    function executeHandleAction(action, data = "") {
+      tk.performTask(
+        "RF 06 - HANDLE ACTIONS",
+        ++state.priority,
+        action,
+        data,
+        "",
+        true,
+        true,
+        "",
+        true
+      );
+    }
+
+    function loadLang(code) {
+      const storedLanguage = tk.shell(
+        `cat "${config.workDir}/src/locales/${code}.json" 2>/dev/null`,
+        false,
+        2000
+      );
+      if (!storedLanguage) throw new Error("");
+      return JSON.parse(storedLanguage);
+    }
+
+    function loadApps() {
+      executeHandleAction("load_apps");
+      return [];
+    }
+
+    function loadTasks() {
+      executeHandleAction("load_tasks");
+      return [];
+    }
+
+    function getCategories() {
+      const data = tk.local("category_data");
+      return validateData(data);
+    }
+
+    function saveCategories(data) {
+      executeHandleAction("update_categories", data);
+    }
+
+    function getRoutines() {
+      const data = tk.local("routine_data");
+      return validateData(data);
+    }
+
+    function saveRoutines(data) {
+      executeHandleAction("update_routines", data);
+    }
+
+    function getSettings() {
+      return tk.global("RF_SETTINGS");
+    }
+
+    function saveSettings(settings) {
+      tk.setGlobal("RF_SETTINGS", settings);
+    }
+
+    return {
+      ...config,
+      loadLang,
+      loadApps,
+      loadTasks,
+      getCategories,
+      saveCategories,
+      getRoutines,
+      saveRoutines,
+      getSettings,
+      saveSettings
+    };
+  };
 
   function detect() {
-    return typeof tk === "undefined" ? WebEnvironment : TaskerEnvironment;
+    return typeof tk === "undefined" ? WebEnvironment() : TaskerEnvironment();
   }
 
   return {
